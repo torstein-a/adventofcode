dafddfb4e29878ffc7497e562e52e29783e4bfbd
Torstein Altin <torstein.altin@bring.com>
2022-12-02 15:23:09 +0100

    Made 02 7 chars shorter by using the devtools $ selector

--- 2022/02/2.js
+++ 2022/02/2.js
@@ -1,1 +1,1 @@
-document.body.innerText.split(/\n/).reduce((a,b)=>a+['','B X','C X','A X','A Y','B Y','C Y','C Z','A Z','B Z'].indexOf(b),0)
+$('*').innerText.split(/\n/).reduce((a,b)=>a+['','B X','C X','A X','A Y','B Y','C Y','C Z','A Z','B Z'].indexOf(b),0)
