965d5f8f3fe7fe415b7cdff37790e3f833759d0d
Torstein Altin <torstein.altin@bring.com>
2022-12-05 22:51:15 +0100

    Remove some chars by keeping string as string and using `includes`

--- 2022/03/1.js
+++ 2022/03/1.js
@@ -1,1 +1,1 @@
-$('*').innerText.split(/\n/).reduce((a,b)=>a+(b&&' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf([...b].find(n=>[...b.substr(-b.length/2)].includes(n)))),0)
+$('*').innerText.split(/\n/).reduce((a,b)=>a+(b&&' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf([...b].find(n=>b.substr(-b.length/2).includes(n)))),0)
