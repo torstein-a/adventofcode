e1cbb0f3557b24adcf86ab328a29ca8a5fa16b34
Torstein Altin <torstein.altin@bring.com>
2022-12-09 14:18:19 +0100

    2

--- 2022/04/1.js
+++ 2022/04/1.js
@@ -1,2 +1,2 @@
 $('*').innerText.split(/\n/).filter(r=>{x=r.split(/,|-/).map(y=>y|0)
-return r&&((x[0]>=x[2]&&x[1]<=x[3])||(x[2]>=x[0]&&x[3]<=x[1]))})
+return (x[0]>=x[2]&&x[1]<=x[3])||(x[2]>=x[0]&&x[3]<=x[1])})
